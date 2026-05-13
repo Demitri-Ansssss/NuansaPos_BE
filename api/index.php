224e2e80becbc40054d6b4bce11bf587558b9da1
Demitri-Ansssss <ansoricahgreges@gmail.com>
2026-05-13 13:10:21 +0700

    editing index.php configuration

--- api/index.php
+++ api/index.php
@@ -1,4 +1,22 @@
-<?php
 
 // Jembatan untuk menjalankan Laravel di Vercel Serverless Functions
-require __DIR__ . '/../public/index.php';
+// require __DIR__ . '/../public/index.php';
+
+<?php
+
+// Jalankan autoload
+require __DIR__ . '/../vendor/autoload.php';
+
+// Inisialisasi Aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Set folder storage ke /tmp agar tidak Error 500 di Vercel
+$app->useStoragePath('/tmp');
+
+// Jalankan Request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
